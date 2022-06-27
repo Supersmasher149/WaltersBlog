7b762ff284684782bce7e76c15a818d61fb63bb9
Penguin <walter.gibbons@gmail.com>
2022-06-27 01:11:15 -0400

    added working post route with working type validation

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -7,9 +7,45 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method === "GET") {
     client.connect();
     const db = client.db("blog");
     const collection = db.collection("posts");
     const result = await collection.find({}).toArray();
     res.json(result);
+  } else if (req.method === "POST") {
+    // Checks if the request body is empty
+    const { Title, Content } = req.body;
+    if (!Title || !Content) {
+      res.status(400).json({
+        error: "Title and Content are required",
+      });
+    }
+
+    // Checks if the reqquest body is the right sizes
+    if (Title.length > 100 || Content.length > 5000) {
+      res.status(400).json({
+        error:
+          "Title must be less than 100 characters and Content must be less than 5000 characters",
+      });
+    }
+
+    client.connect();
+    const db = client.db("blog");
+    const collection = db.collection("posts");
+
+    // If Title already exists, return error
+    const findResult = await collection.find({ Title }).toArray();
+    console.log(findResult);
+    if (findResult.length > 0) {
+      res.status(400).json({
+        error: "Title already exists",
+      });
+    } else if (findResult.length === 0) {
+      const insertResult = await collection.insertOne({ Title, Content });
+      res.json(insertResult);
+    }
+  } else {
+    res.status(405).end();
+  }
 }
