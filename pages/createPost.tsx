b23cdccdbc18f4ac382d3dfcfd2d817825060d7e
Penguin <walter.gibbons@gmail.com>
2022-06-27 01:11:27 -0400

    added page to create posts

--- pages/createPost.tsx
+++ pages/createPost.tsx
@@ -1,9 +1,55 @@
+import { Formik, Form, Field } from "formik";
 import type { NextPage } from "next";
+import styles from "../styles/CreatePost.module.css";
 
 const CreatePost: NextPage = () => {
   return (
     <div>
-      <h1>Create Post</h1>
+      <Formik
+        initialValues={{ Title: "", Content: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            fetch("/api/posts", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(values),
+            });
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <div className={styles.Form}>
+            <Form>
+              <div className={styles.Inputs}>
+                <Field
+                  className={styles.Title}
+                  type="text"
+                  name="Title"
+                  placeholder="Title"
+                />
+                <Field
+                  className={styles.Body}
+                  as="textarea"
+                  name="Content"
+                  placeholder="Content"
+                />
+              </div>
+              <div>
+                <button
+                  className={styles.Submit}
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Submit
+                </button>
+              </div>
+            </Form>
+          </div>
+        )}
+      </Formik>
     </div>
   );
 };
